1bd7a259512d0ec9578376d67df881bc7c3ff928
WeRiano <williamfors13@gmail.com>
2022-01-08 15:59:36 +0100

    Docker deployment networking

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -70,10 +70,8 @@ MIDDLEWARE = [
 CORS_ALLOW_ALL_ORIGINS = False
 
 CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:8001",
-    "http://localhost:8001",
-    "http://127.0.0.1:8000",
-    "http://localhost:8000"
+    "http://" + os.environ.get("FRONTEND_IP", "127.0.0.1") + ":8001",
+    "http://" + os.environ.get("SIMULATION_IP", "127.0.0.1") + ":8000"
 ]
 
 CORS_ALLOWED_METHODS = [
